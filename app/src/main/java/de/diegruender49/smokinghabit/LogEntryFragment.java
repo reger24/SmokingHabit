4f42c4b669485dd236d2e392cafb8313af369177
reger24 <11603289+reger24@users.noreply.github.com>
2022-12-19 16:40:56 +0100

    refactor logentryfragment

--- app/src/main/java/de/diegruender49/smokinghabit/LogEntryFragment.java
+++ app/src/main/java/de/diegruender49/smokinghabit/LogEntryFragment.java
@@ -24,35 +24,12 @@ import de.diegruender49.smokinghabit.placeholder.PlaceholderContent;
  */
 public class LogEntryFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
     public LogEntryFragment() {
-    }
-
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static LogEntryFragment newInstance(int columnCount) {
-        LogEntryFragment fragment = new LogEntryFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -64,13 +41,9 @@ public class LogEntryFragment extends Fragment {
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
-            // show last smokelog entries
+            // get last smokelog entries
             Calendar cal = Calendar.getInstance();
             cal.add(Calendar.HOUR, - (24 * 2));
             SmokeStatistic smstat = new SmokeStatistic(getContext());
